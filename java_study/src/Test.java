d751c5fa668afe881db7c11d154a4e78fafcf818
lengdian <574463726@qq.com>
2016-12-01 15:13:38 +0800

    test

--- java_study/src/Test.java
+++ java_study/src/Test.java
@@ -1,28 +1,9 @@
-import java.lang.ref.SoftReference;
-
-public class Test {
-    public static void main(String[] args) {
-//        System.out.println("hello world");
-
-        Test test=new Test();
-        test.say();
-
-        SoftReference<Test> sr=new SoftReference<Test>(test);
-
-        System.out.println(test);
-
-        test=null;
-
-        if(sr.get()!=null){
-            Test t1=sr.get();
-
-            t1.say();
-        }
-
-
+public class Test
+{
+    @SuppressWarnings("unCheck")
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
     }
 
-    private void say(){
-        System.out.println("hi");
-    }
 }
