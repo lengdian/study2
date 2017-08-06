d6e75f29d5678cace66bf4d1d7f4063b0dacc6cf
lengdian <574463726@qq.com>
2017-08-06 22:20:27 +0800

    tt

---
+++ webpack_study/app/main.js
@@ -0,0 +1,4 @@
+//main.js
+var greeter = require('./Greeter.js');
+
+document.getElementById('root').appendChild(greeter());
---
+++ webpack_study/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    },
+
+    module:{
+        loaders:[
+            {
+                test:'/\.json$/',
+                loader:'json-loader'
+            }
+        ]
+    },
+
+    devtool: 'eveal-source-map',
+
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        colors: true,//终端中输出结果为彩色
+        historyApiFallback: true,//不跳转
+        inline: true,//实时刷新
+        port: 9000
+    }
+};
