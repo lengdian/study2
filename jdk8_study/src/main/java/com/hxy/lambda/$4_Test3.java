e353a2ba0ef2d2b17804323676ef005b5985890d
lengdian <574463726@qq.com>
2017-10-24 12:22:54 +0800

    jdk8

--- jdk8_study/src/main/java/com/hxy/lambda/$4_Test3.java
+++ jdk8_study/src/main/java/com/hxy/lambda/$4_Test3.java
@@ -8,29 +8,25 @@ public class $4_Test3
 {
     public static void main(String[] args)
     {
-//        TheInterface ti=() -> {};
-//        System.out.println(ti.getClass().getInterfaces()[0]);
-//
-//        TheInterface2 ti2=() -> {};
-//        System.out.println(ti2.getClass().getInterfaces()[0]);
-//
-//        new Thread(() -> System.out.println("hello world")).start();
+        //lambda表达式返回的是对象，具体是什么类型的对象需要上下文信息才能判断
+        //        TheInterface ti=() -> {};
+        //        System.out.println(ti.getClass().getInterfaces()[0]);
+        //        TheInterface2 ti2=() -> {};
+        //        System.out.println(ti2.getClass().getInterfaces()[0]);
+        //
+        //        new Thread(() -> System.out.println("hello world")).start();
 
         List<String> list = Arrays.asList("hello", "world", "hello world");
         List<String> list2 = new ArrayList<>();
-        // 传统复制方法
-//        list.forEach(item -> list2.add(item.toUpperCase()));
-//        list2.forEach(item -> System.out.println(item));
+        // lambda表达式复制方法
+        // list.forEach(item -> list2.add(item.toUpperCase()));
+        // list2.forEach(item -> System.out.println(item));
 
         //流（stream）+lambda表达式
         list.stream().map(item -> item.toUpperCase()).forEach(item -> System.out.println(item));
 
         //流+方法引用
         list.stream().map(String::toUpperCase).forEach(System.out::println);
-
-
-
-//        list.forEach(item -> System.out.println(item.toUpperCase()));
 
     }
 }
