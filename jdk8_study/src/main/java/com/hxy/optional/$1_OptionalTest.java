9cbd22030e40062b223ff7f50e79bf96f68c3942
lengdian <574463726@qq.com>
2017-11-06 11:21:11 +0800

    jdk8

--- jdk8_study/src/main/java/com/hxy/optional/$1_OptionalTest.java
+++ jdk8_study/src/main/java/com/hxy/optional/$1_OptionalTest.java
@@ -7,17 +7,16 @@ public class $1_OptionalTest
 {
     public static void main(String[] args)
     {
-        //Optional<String> optional = Optional.of("hello");
-        //Optional<String> optional = Optional.empty();
-        Optional<String> optional = Optional.ofNullable("hello");
+        Optional<String> optional = Optional.of("hello");
+        //        Optional<String> optional = Optional.empty();
+        //        Optional<String> optional = Optional.ofNullable("hello");
 
 
-        //固有思维方式
-        if (optional.isPresent())
-        {
-            System.out.println(optional.get());
-            System.out.println("----------------------");
-        }
+        //固有思维方式，不推荐的方式
+        //        if (optional.isPresent())
+        //        {
+        //            System.out.println(optional.get());
+        //        }
 
         //函数式风格:推荐的Optional使用方式
         optional.ifPresent(item -> System.out.println(item));
