0a6d3901bf64bc76a5fe67417df08dbccf9b8f80
lengdian <574463726@qq.com>
2017-12-20 11:32:08 +0800

    jdk8

--- jdk8_study/src/main/java/com/hxy/comparator/$1_MyComparatorTest.java
+++ jdk8_study/src/main/java/com/hxy/comparator/$1_MyComparatorTest.java
@@ -12,15 +12,33 @@ public class $1_MyComparatorTest
         List<String> list = Arrays.asList("nihao", "hello", "world", "welcome");
 
         //        Collections.sort(list);
+
         //一层Comparator,能根据上下文直接推断出类型
-        Collections.sort(list, (item1, item2) -> item1.length() - item2.length());
+        //        Collections.sort(list, (item1, item2) -> item1.length() - item2.length());
+
         //        Collections.sort(list, Comparator.comparingInt(String::length).reversed());
+        //        list.sort(Comparator.comparingInt(String::length));
 
         //两层Comparator，Comparator.comparingInt((String item) -> item.length())不能根据上下文直接推断出类型，离上下文太远
         //需要显示指定String类型，编译器无法进行类型推断
-        Collections.sort(list, Comparator.comparingInt((String item) -> item.length()).reversed());
+        //        Collections.sort(list, Comparator.comparingInt((String item) -> item.length()).reversed());
 
-        Collections.sort(list, Comparator.comparingInt((Object item) -> 1).reversed());
+        //        Collections.sort(list, Comparator.comparingInt((Object item) -> 1).reversed());
+
+        //多级比较
+        //thenComparing:如果前一级比较结果相等，才使用thenComparing提供的比较器进行比较！！！
+        //        Collections.sort(list, Comparator.comparingInt(String::length).thenComparing(String
+        // .CASE_INSENSITIVE_ORDER));
+
+        //        Collections.sort(list, Comparator.comparingInt(String::length).thenComparing(Comparator.comparing
+        //                (String::toLowerCase, Comparator.reverseOrder())));
+
+        //        Collections.sort(list, Comparator.comparingInt(String::length).reversed().thenComparing(Comparator
+        // .comparing
+        //                (String::toLowerCase, Comparator.reverseOrder())));
+
+        Collections.sort(list, Comparator.comparingInt(String::length).reversed().thenComparing(Comparator.comparing
+                (String::toLowerCase, Comparator.reverseOrder())).thenComparing(Comparator.reverseOrder()));
 
         System.out.println(list);
 
